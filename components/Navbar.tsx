7aec574d685cc7b88e7bf03b48a87ac64e695968
Mateus-Band <al.mateus.torres@impatech.edu.br>
2026-04-09 21:27:01 -0300

    logos and colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,19 +4,30 @@ import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <header className="w-full py-4" style={{ backgroundColor: 'var(--color-primary)', margin: '0' }}>
-      <div className="flex items-center" style={{ paddingLeft: '24px', gap: '24px' }}>
+    <header 
+      className="py-4 flex items-center" 
+      style={{ backgroundColor: 'var(--color-secondary)', margin: '0', paddingLeft: '24px', paddingRight: '24px' }}
+    >
+      <div className="flex items-center" style={{ gap: '24px' }}>
         <Image
-          src="/icon.svg"
+          src="/logo.png"
           alt="Axioma Icon"
           width={120}
-          height={100}
+          height={120}
           priority
         />
         <div>
           <h1 className="text-5xl font-bold">Axioma</h1>
           <p className="mt-4 text-gray-400">Impatech — em breve</p>
         </div>
+      </div>
+      <div className="flex-1 flex items-center justify-end">
+        <Image
+          src="/icon.svg"
+          alt="Logo IMPA Tech"
+          width={120}
+          height={100}
+        />
       </div>
     </header>
   );
